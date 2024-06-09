44aef2b2d2905f867d45601be63e7206f4e5277c
abigailqm <abigailquinnsa@gmail.com>
2024-06-09 17:16:10 +0700

    Create bot.py

---
+++ bot.py
@@ -0,0 +1,30 @@
+import discord
+from discord.ext import commands
+
+# membaca token dari file
+with open("token.txt", "r") as f:
+    token = f.read()
+
+intents = discord.Intents.default()
+intents.message_content = True
+
+bot = commands.Bot(command_prefix='$', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.command()
+async def hello(ctx):
+    await ctx.send(f'Hi! I am a bot {bot.user}!')
+
+
+@bot.command()
+async def heh(ctx, count_heh = 5):
+    await ctx.send("he" * count_heh)
+
+@bot.command()
+async def passw(ctx, panjang = 5):
+    await ctx.send(genn_pass(panjang))
+
+bot.run(token)
